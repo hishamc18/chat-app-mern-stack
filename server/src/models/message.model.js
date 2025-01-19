9713b86edb86bd01c8cc6fe066857d4a71eaa955
Hisham C <hishamc18@gmail.com>
2025-01-19 21:04:56 +0530

    created model for message

---
+++ server/src/models/message.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const messageScheme = new mongoose.Schema({
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    text: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+}, { timestamps })
+
+const Message = mongoose.model("Message", messageScheme)
+
+export default Message;
