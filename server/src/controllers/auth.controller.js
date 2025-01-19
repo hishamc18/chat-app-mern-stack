ce227c04bba8bacaeee47f4b9049958d726d20e4
Hisham C <hishamc18@gmail.com>
2025-01-19 20:40:53 +0530

    controller for update profilePic

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from '../lib/cloudinary.js'
 import { generateToken } from '../lib/utils.js'
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
@@ -89,6 +90,26 @@ export const logout = (req, res) => {
         res.status(200).json({ message: "Logged out successfully" })
     } catch (error) {
         console.log('error in logout controller', error.message);
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePic } = req.body
+        const userId = req.user._id
+
+        if (!profilePic) {
+            res.status(400).json({ message: "Profile Picture is required" })
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+
+        const updatedUser = await User.find(userId, { profilePic: uploadResponse.secure_url }, { new: true })
+
+        res.status(200).status(updatedUser)
+    } catch (error) {
+        console.log('error in updateProfile controller', error.message);
         res.status(500).json({ message: "Internal Server Error" })
     }
 }
