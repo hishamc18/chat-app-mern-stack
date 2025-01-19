826ccfa356676a88c5c5e8b33b5d65dacff0dc70
Hisham C <hishamc18@gmail.com>
2025-01-19 22:08:58 +0530

    pages and components were added, succesfully set up the axiosInstance

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import SignUpPage from './pages/SignUpPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div className='text-yellow-500'>
-      Hello how are you
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/settings' element={<SettingsPage />} />
+        <Route path='/profile' element={<ProfilePage />} />
+      </Routes>
     </div>
   )
 }
