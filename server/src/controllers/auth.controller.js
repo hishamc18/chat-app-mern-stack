8e1744633cc53899b17c09362be06f92e3815389
Hisham C <hishamc18@gmail.com>
2025-01-19 20:57:38 +0530

    controller for getting authenticated user

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -113,3 +113,12 @@ export const updateProfile = async (req, res) => {
         res.status(500).json({ message: "Internal Server Error" })
     }
 }
+
+export const checkAuth = async (req, res) => {
+    try {
+        res.status(200).json(req.user)
+    } catch (error) {
+        console.log('error in checkAuth controller', error.message);
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
